251d310ca5de2c7f8be303ebed16ee4caf6a0d79
ciptacoding <ciptacoding@gmail.com>
2022-08-21 11:36:50 +0800

    gas

---
+++ 02 - JS DOM/07-DomTravesal/travesal.js
@@ -0,0 +1,17 @@
+// const btnClose = document.querySelectorAll(".close");
+
+// for (let i = 0; i < btnClose.length; i++) {
+//   btnClose[i].addEventListener("click", function (e) {
+//     e.target.parentElement.style.display = "none";
+//   });
+// }
+
+/*<-------------------------Pembatas------------------------------> */
+// cara lebih efektif
+const btnClose = document.querySelectorAll(".close");
+
+btnClose.forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.target.parentElement.style.display = "none";
+  });
+});
---
+++ 02 - JS DOM/07-DomTravesal/travesalMethod.js
@@ -0,0 +1,17 @@
+// travesal = penelusuran
+const nama = document.querySelector(".nama");
+
+// card
+console.log(nama.parentElement);
+
+// card
+console.log(nama.parentNode);
+
+// container
+console.log(nama.parentElement.parentElement);
+
+// telp
+console.log(nama.nextElementSibling);
+
+// img
+console.log(nama.previousElementSibling);
